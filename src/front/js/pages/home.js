01b144bc1a23706da29f9b335831ac1ccbb98697
CesarMendez1 <152318003+CesarMendez1@users.noreply.github.com>
2024-06-27 22:15:53 +0000

    adelantado formulario project

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-
+	const [data,setData]=useState({
+		email:"",
+		username:"",
+		name:"",
+		password:""
+	})
+	const imput=(e)=>{
+		const {name,value}=e.target;
+		setData(previewdata=>({
+			...previewdata,[name]:value
+		}));
+	}
+	const registro=async(e)=>{
+		e.preventDefault();
+		const resultado= await actions.registro(data.email,data.username,data.password,data.name)
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+		<div className=" mt-5 container">
+			<h1>Registro</h1>
+			<form className="row g-3" onSubmit={registro}>
+				<div className="col-12">
+					<label htmlFor="inputEmail4" className="form-label">Email</label>
+					<input type="email" className="form-control" id="inputEmail4" name="email" value={data.email} onChange={imput} />
+				</div>
+				<div className="col-12">
+					<label htmlFor="inputPassword4" className="form-label">Password</label>
+					<input type="password" className="form-control" id="inputPassword4" name="password" value={data.password} onChange={imput} />
+				</div>
+				<div className="col-12">
+					<label htmlFor="inputAddress" className="form-label">Username</label>
+					<input type="text" className="form-control" id="inputAddress" name="username" value={data.username} onChange={imput} />
+				</div>
+				<div className="col-12">
+					<label htmlFor="inputAddress2" className="form-label">Name</label>
+					<input type="text" className="form-control" id="inputAddress2" name="name" value={data.name} onChange={imput} />
+				</div>
+
+
+
+
+				<div className="col-12">
+					<button type="submit" className="btn btn-primary">Sign in</button>
+				</div>
+			</form>
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -17,8 +17,27 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			registro:async (email,username,password,name) => {
+					try{
+						const response=await fetch (process.env.BACKEND_URL + "api/registro",{
+							method:"POST",
+							headers:{
+								"Content-Type":"application/json"
+							},
+							body:JSON.stringify({
+								email:email,
+								username:username, 
+								password:password,
+								name:name 
+							})
+						});
+						if (!response.ok){
+							throw new Error("falla al crear usuario")
+						}
+						const data= await response.json()
+						return data
+					} catch(error)
+					{console.error(error)}	
 			},
 
 			getMessage: async () => {
